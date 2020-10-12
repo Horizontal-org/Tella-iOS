1e3e72c7683477cebe4087be22180bff0fd3129a
Bruno Pastre <pastre68@gmail.com>
2020-10-12 10:53:30 -0300

    Better functions

--- Tella/audio/RecordingAudioManager.swift
+++ Tella/audio/RecordingAudioManager.swift
@@ -13,25 +13,13 @@ class RecordingAudioManager: AudioManager {
     private var recorder: AVAudioRecorder!
     
     func startRecording() {
-        let session = AVAudioSession.sharedInstance()
-        
-        do {
-            try session.setCategory(.playAndRecord, mode: .default)
-            try session.setActive(true)
-        } catch let error {
-            // @TODO Delegate this to the ViewModel
-            print("Error is", error)
-            return
-        }
-        
         guard
-            let docPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            self.configureSession(),
+            let fileName = self.getFileName()
         else {
-            // @TODO Delegate this to the ViewModel
+            // @TODO Delegate this to the viewModel
             return
         }
-        
-        let fileName = docPath.appendingPathComponent("\(Int(Date().timeIntervalSince1970)).m4a")
         
         let settings = [
             AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
@@ -66,5 +54,25 @@ class RecordingAudioManager: AudioManager {
         
     }
     
+    fileprivate func getFileName() -> URL? {
+        
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("\(Int(Date().timeIntervalSince1970)).m4a")
+    }
+    
+    fileprivate func configureSession() -> Bool {
+        let session = AVAudioSession.sharedInstance()
+        
+        do {
+            try session.setCategory(.playAndRecord, mode: .default)
+            try session.setActive(true)
+        } catch let error {
+            // @TODO Delegate this to the ViewModel
+            print("Error is", error)
+            return false
+        }
+        
+        return true
+        
+    }
     
 }
