3b715fd50179cf4358f0394eeb840d10f320d122
Ahlem Jarrar <ahlem.jarrar@hotmail.com>
2021-05-01 02:00:33 +0100

    Complete DragView + Confirm bottom sheet

---
+++ Tella/Common/bottomsheet/ConfirmBottomSheet.swift
@@ -0,0 +1,83 @@
+//
+//  ConfirmBottomSheet.swift
+//  Tella
+//
+//  Created by Ahlem on 30/04/2021.
+//  Copyright © 2021 INTERNEWS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ConfirmBottomSheet : View {
+    var titleText = ""
+    var msgText = ""
+    var cancelText = ""
+    var actionText = ""
+    @Binding var isPresented: Bool
+    var didConfirmAction : () -> ()
+    var body: some View {
+        
+        VStack(alignment: .leading, content: {
+            Spacer()
+            Text(self.titleText)
+                .foregroundColor(.white)
+                .font(Font.custom("open-sans.regular", size: 20))
+                .fontWeight(.regular)
+                .padding(.top, 20)
+                .padding(.horizontal, 20)
+            
+            Text(self.msgText)
+                .foregroundColor(.white)
+                .font(Font.custom("open-sans.regular", size: 14))
+                .fontWeight(.light)
+                .padding(20)
+            
+            HStack(alignment: .lastTextBaseline ){
+                Spacer()
+                Button(action: {self.isPresented = false}){
+                    Text(self.cancelText)
+                }.buttonStyle(ButtonSheetStyle())
+                
+                Button(action: {self.didConfirmAction()}){
+                    Text(self.actionText)
+                }.buttonStyle(ButtonSheetStyle())
+                
+            }
+            .padding(.trailing, 20)
+            .padding(.bottom, 40)
+        })
+        .background(Color("PrimaryColor"))
+        .cornerRadius(25)
+    }
+}
+
+struct ButtonSheetStyle: ButtonStyle {
+
+  func makeBody(configuration: Self.Configuration) -> some View {
+    configuration.label
+      .foregroundColor(configuration.isPressed ? Color.red : Color.white)
+      .padding(.leading, 20)
+  }
+}
+struct TestView: View {
+    @State var show = true
+    
+    var body: some View {
+        
+        ZStack {
+            Button(action: {
+                    self.show.toggle()}){
+                Text("Action sheet")
+            }
+            DragView(modalHeight: 220, isShown: $show){
+                ConfirmBottomSheet(titleText: "Delete file?", msgText: "The selected file will be permanenetly delated from your vault.",cancelText: "CANCEL",actionText: "DELETE",isPresented: $show,didConfirmAction: {})
+            }
+        }
+    }
+}
+
+struct ConfirmBottomSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
---
+++ Tella/Common/bottomsheet/DragView.swift
@@ -0,0 +1,116 @@
+//
+//  DragView.swift
+//  BottomSheetTest
+//
+//  Created by Ahlem on 29/04/2021.
+//
+
+import SwiftUI
+
+enum DragState {
+    case inactive
+    case dragging(translation: CGSize)
+    
+    var translation: CGSize {
+        switch self {
+        case .inactive:
+            return .zero
+        case .dragging(let translation):
+            return translation
+        }
+    }
+    
+    var isDragging: Bool {
+        switch self {
+        case .inactive:
+            return false
+        case .dragging:
+            return true
+        }
+    }
+}
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+struct DragView<Content: View> : View {
+    @GestureState private var dragState = DragState.inactive
+    var modalHeight:CGFloat
+    @Binding var isShown:Bool
+    var color:Color = .white
+    
+    private func onDragEnded(drag: DragGesture.Value) {
+        let dragThreshold = modalHeight * (2/3)
+        if drag.predictedEndTranslation.height > dragThreshold || drag.translation.height > dragThreshold{
+            isShown = false
+        }
+    }
+    
+    var content: () -> Content
+    var body: some View {
+        let drag = DragGesture()
+            .updating($dragState) { drag, state, transaction in
+                state = .dragging(translation: drag.translation)
+            }
+            .onEnded(onDragEnded)
+        return Group {
+            ZStack {
+                //Background
+                Spacer()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height+20)
+                    .background(isShown ? Color.black.opacity( 0.5 * fraction_progress(lowerLimit: 0, upperLimit: Double(modalHeight), current: Double(dragState.translation.height), inverted: true)) : Color.clear)
+                    .animation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
+                    .gesture(
+                        TapGesture()
+                            .onEnded { _ in
+                                UIApplication.shared.endEditing()
+                                self.isShown = false
+                            }
+                    )
+                
+                //Foreground
+                VStack{
+                    Spacer()
+                    ZStack{
+                        color.opacity(1.0)
+                            .frame(width: UIScreen.main.bounds.size.width, height:modalHeight)
+                            .cornerRadius(25)
+                            .shadow(radius: 25)
+                        self.content()
+                            
+                            .padding(.bottom, 0)
+                            .frame(width: UIScreen.main.bounds.size.width, height:modalHeight)
+                            .clipped()
+                    }
+                    .offset(y: isShown ? ((self.dragState.isDragging && dragState.translation.height >= 1) ? dragState.translation.height : 0) : modalHeight)
+                    .animation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
+                    .gesture(drag)
+                }
+            }.edgesIgnoringSafeArea(.all)
+            .zIndex(.infinity)
+            
+        }
+    }
+}
+
+func fraction_progress(lowerLimit: Double = 0, upperLimit:Double, current:Double, inverted:Bool = false) -> Double{
+    var val:Double = 0
+    if current >= upperLimit {
+        val = 1
+    } else if current <= lowerLimit {
+        val = 0
+    } else {
+        val = (current - lowerLimit)/(upperLimit - lowerLimit)
+    }
+    
+    if inverted {
+        return (1 - val)
+        
+    } else {
+        return val
+    }
+    
+}
+
