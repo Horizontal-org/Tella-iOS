7404737daca9377a7415a49c45e9414095f7fc7b
Ahlem Jarrar <ahlem.jarrar@hotmail.com>
2021-06-07 16:28:42 +0100

    Add forms header

---
+++ Tella/Views/widgets/HeaderView.swift
@@ -0,0 +1,51 @@
+//
+//  HeaderView.swift
+//  Tella
+//
+//  Created by Ahlem on 07/06/2021.
+//  Copyright © 2021 INTERNEWS. All rights reserved.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var onRefreshClick : () -> ()
+    var onHelpClick : () -> ()
+    var onNewFormClick : () -> ()
+    var body: some View {
+        HStack(alignment: .center ,spacing: 20){
+            Button(action: {
+                self.onNewFormClick()
+                print("button fill pressed")
+            }) {
+                Image("fill-icon")
+                    .renderingMode(.original)
+            }
+            
+            Button(action: {
+                self.onRefreshClick()
+                print("button refresh pressed")
+            }) {
+                Image("refresh-icon")
+                    .renderingMode(.original)
+            }
+            
+            Button(action: {
+                self.onHelpClick()
+                print("button help pressed")
+            }) {
+                Image("help-icon")
+                    .renderingMode(.original)
+            }.padding(.trailing, 20)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight : 56,alignment: .trailing)
+        .background(Color(Styles.Colors.backgroundMain))
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView(onRefreshClick: {}, onHelpClick:{}, onNewFormClick: {})
+    }
+}
