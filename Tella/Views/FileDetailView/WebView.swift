d3aa93641d1fdd12021bec6e4c4eed11aa1249b3
Ahlem Jarrar <ahlem.jarrar@hotmail.com>
2021-07-03 22:30:19 +0100

    Add webView implementation

---
+++ Tella/Views/FileDetailView/WebView.swift
@@ -0,0 +1,43 @@
+//
+//  WebView.swift
+//  Tella
+//
+//  Created by Ahlem on 30/06/2021.
+//  Copyright © 2021 INTERNEWS. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebViewer: View {
+    var url: String
+    var body: some View {
+        Webview(url: URL(string: url)!)
+    }
+}
+
+struct Webview: UIViewRepresentable {
+    var url: URL
+    
+    func makeUIView(context: UIViewRepresentableContext<Webview>) -> WKWebView {
+        let webview = WKWebView()
+        
+        let request = URLRequest(url: self.url, cachePolicy: .returnCacheDataElseLoad)
+        webview.load(request)
+        
+        return webview
+    }
+    
+    func updateUIView(_ webview: WKWebView, context: UIViewRepresentableContext<Webview>) {
+        let request = URLRequest(url: self.url, cachePolicy: .returnCacheDataElseLoad)
+        webview.load(request)
+    }
+}
+
+
+struct WebViewerViewer_Previews: PreviewProvider {
+    static var previews: some View {
+        WebViewer(url: "https://google.com")
+    }
+}
