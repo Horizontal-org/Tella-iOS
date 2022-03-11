7f8b0b85f7ba2e6652a0dbf99611b54602084cd2
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2022-03-11 16:42:28 +0000

    File list : Update Grid view

--- Tella/Views/HomeView/Views/FileListView/File list/FileGridItem.swift
+++ Tella/Views/HomeView/Views/FileListView/File list/FileGridItem.swift
@@ -11,65 +11,59 @@ import SwiftUI
 struct FileGridItem: View {
     
     var file: VaultFile
-//    @State var isSelected : Bool = false
 
     @EnvironmentObject var appModel: MainAppModel
     @EnvironmentObject var fileListViewModel : FileListViewModel
-
+    
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
+        
+        ZStack {
+            
+            file.recentGridImage
+            
+            VStack(alignment: .trailing) {
                 
-                file.recentGridImage
-                    .frame(width: 80, height: 80)
-                
-                VStack(alignment: .trailing) {
-                    
+                Spacer()
+                HStack {
                     Spacer()
-                    HStack {
-                        Spacer()
+                    
+                    Button {
+                        fileListViewModel.fileActionMenuType = .single
+                        fileListViewModel.showingFileActionMenu = true
+                        fileListViewModel.currentSelectedVaultFile = file
                         
-                        Button {
-                            fileListViewModel.fileActionMenuType = .single
-                            fileListViewModel.showingFileActionMenu = true
-                            fileListViewModel.currentSelectedVaultFile = file
-                            
-                        } label: {
-                            Image("files.more")
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                                .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 0))
-                        }
-                    }
-                }
-                
-                if fileListViewModel.selectingFiles {
-                    Color.black.opacity(0.32)
-                        .frame(width: 80, height: 80)
-                        .onTapGesture {
-                            fileListViewModel.updateSelection(for: file)
-                        }
-                    HStack() {
-                        
-                        VStack(alignment: .leading) {
-                            Image(fileListViewModel.getStatus(for: file) ? "files.selected" : "files.unselected")
-                                .resizable()
-                                .frame(width: 15, height: 15)
-                                .padding(EdgeInsets(top: 6, leading: 6, bottom: 0, trailing: 0))
-                            Spacer()
-                            
-                        }.onTapGesture {
-                            fileListViewModel.updateSelection(for: file)
-                        }
-                        Spacer()
+                    } label: {
+                        Image("files.more")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 0))
                     }
                 }
             }
-            .background((fileListViewModel.getStatus(for: file) && fileListViewModel.selectingFiles) ? Color.white.opacity(0.16) : Styles.Colors.backgroundMain)
             
-        } .frame(width: 80, height: 80)
+            if fileListViewModel.selectingFiles {
+                Color.black.opacity(0.32)
+                    .onTapGesture {
+                        fileListViewModel.updateSelection(for: file)
+                    }
+                HStack() {
+                    
+                    VStack(alignment: .leading) {
+                        Image(fileListViewModel.getStatus(for: file) ? "files.selected" : "files.unselected")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                            .padding(EdgeInsets(top: 6, leading: 6, bottom: 0, trailing: 0))
+                        Spacer()
+                        
+                    }.onTapGesture {
+                        fileListViewModel.updateSelection(for: file)
+                    }
+                    Spacer()
+                }
+            }
+        }
+        .background((fileListViewModel.getStatus(for: file) && fileListViewModel.selectingFiles) ? Color.white.opacity(0.16) : Styles.Colors.backgroundMain)
     }
-    
 }
 
 struct FileGridItem_Previews: PreviewProvider {
@@ -77,7 +71,7 @@ struct FileGridItem_Previews: PreviewProvider {
         FileGridItem(file: VaultFile.stub(type: .folder))
             .environmentObject(MainAppModel())
             .environmentObject(FileListViewModel.stub())
-
+        
     }
 }
 
--- Tella/Views/HomeView/Views/FileListView/File list/FileItemsView.swift
+++ Tella/Views/HomeView/Views/FileListView/File list/FileItemsView.swift
@@ -9,15 +9,14 @@ struct FileItemsView: View {
     @EnvironmentObject var fileListViewModel : FileListViewModel
     var files : [VaultFile]
     
-    
-    @available(iOS 14.0, *)
     private var gridLayout: [GridItem] {
-        [GridItem(.fixed(80),spacing: 6),
-         GridItem(.fixed(80),spacing: 6),
-         GridItem(.fixed(80),spacing: 6),
-         GridItem(.fixed(80),spacing: 6)]
+        [GridItem(.adaptive(minimum: 80),spacing: 6)]
     }
-
+    
+    private var maxHeight: CGFloat {
+        return (UIScreen.screenWidth / 4) - 6
+    }
+    
     var body: some View {
         if fileListViewModel.viewType == .list {
             itemsListView
@@ -36,7 +35,7 @@ struct FileItemsView: View {
                     case .folder:
                         
                         FileGridItem(file: file)
-                        
+                            .frame( minHeight: maxHeight)
                             .onTapGesture {
                                 fileListViewModel.rootFile = file
                                 fileListViewModel.folderArray.append(file)
@@ -44,7 +43,7 @@ struct FileItemsView: View {
                     default:
                         ZStack {
                             FileGridItem(file: file)
-                            
+                                .frame( minHeight: maxHeight)
                                 .onTapGesture {
                                     fileListViewModel.showFileDetails = true
                                     self.fileListViewModel.currentSelectedVaultFile = file
@@ -87,6 +86,9 @@ struct FileItemsView: View {
 
 struct FileItemsView_Previews: PreviewProvider {
     static var previews: some View {
-        FileItemsView(files: [VaultFile.stub(type: .folder)])
+        FileItemsView(files: [VaultFile.stub(type: .folder),
+                              VaultFile.stub(type: .folder)])
+            .environmentObject(FileListViewModel.stub())
+        
     }
 }
