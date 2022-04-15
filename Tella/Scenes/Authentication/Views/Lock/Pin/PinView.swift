9b23fd093a1bf08fded45c83228d8994efed6763
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2022-04-15 16:20:03 +0100

    T-iOS-8 - Add visual feedback : buttons on the PIN unlock screen

--- Tella/Scenes/Authentication/Views/Lock/Pin/PinView.swift
+++ Tella/Scenes/Authentication/Views/Lock/Pin/PinView.swift
@@ -30,26 +30,27 @@ struct PinView: View {
                         self.delete(pin: item.text)
                     } label: {
                         Image(item.imageName)
+                            .frame(width: 70, height: 70)
                     }
                     .disabled(!(self.fieldContent.count>0))
-                    .frame(width: 70, height: 70)
+                    .buttonStyle(PinButtonStyle(enabled: self.fieldContent.count>0))
                     
                 case .number:
                     Button {
                         self.appendPin(pin: item.text)
                     } label: {
                         Text(item.text)
-                    }.frame(width: 70, height: 70)
-                        .buttonStyle(PinButtonStyle())
+                            .frame(width: 70, height: 70)
+                    }
+                    .buttonStyle(PinButtonStyle(enabled: true))
                     
                 case .done:
                     Button {
                         action?()
                     } label: {
                         Text(item.text)
-                            .foregroundColor(self.fieldContent.passwordValidator() ? .white : .white.opacity(0.24) )
-                    }.buttonStyle(PinButtonStyle())
-                        .frame(width: 70, height: 70)
+                            .frame(width: 70, height: 70)
+                    }.buttonStyle(PinButtonStyle(enabled: self.fieldContent.passwordValidator()))
                         .disabled(!self.fieldContent.passwordValidator())
                     
                 default:
@@ -70,10 +71,14 @@ struct PinView: View {
 
 struct PinButtonStyle : ButtonStyle {
     
+    var enabled : Bool
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(.custom(Styles.Fonts.lightRobotoFontName, size: 32))
-            .foregroundColor(configuration.isPressed ? .gray : .white)
+            .foregroundColor(configuration.isPressed || !enabled ? .white.opacity(0.24) : .white)
+            .background(configuration.isPressed ? Color.white.opacity(0.24) : Color.clear)
+            .clipShape(Circle())
     }
 }
 
