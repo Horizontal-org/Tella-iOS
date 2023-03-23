60072700a31c7df17e6ce564642aad922b342ebf
Gustavo <valbuena52@gmail.com>
2023-03-23 13:15:25 -0300

    delete photos and documents logic

--- Tella/Components/ImagePickerView.swift
+++ Tella/Components/ImagePickerView.swift
@@ -44,8 +44,7 @@ class ImageCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerC
         let mediaType = info[UIImagePickerController.InfoKey.mediaType] as AnyObject
         debugLog("\(mediaType)")
         let mediaURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL
-        let imageURL = info[UIImagePickerController.InfoKey.imageURL] as? URL
-
+        let imageURL = info[UIImagePickerController.InfoKey.referenceURL] as? URL
         if mediaType as! CFString == kUTTypeImage {
             guard let unwrapImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
                 return
--- Tella/Scenes/HomeView/Views/FileListView/Add files/PhotoVideoViewModel.swift
+++ Tella/Scenes/HomeView/Views/FileListView/Add files/PhotoVideoViewModel.swift
@@ -6,6 +6,7 @@
 import Foundation
 import UIKit
 import SwiftUI
+import Photos
 
 class PhotoVideoViewModel : ObservableObject {
     
@@ -29,6 +30,10 @@ class PhotoVideoViewModel : ObservableObject {
                                                                  to: self.mainAppModel.vaultManager.root,
                                                                  type: type,
                                                                  folderPathArray: self.folderPathArray)
+                
+                if mainAppModel.importOption == .deleteOriginal {
+                    removeFiles(files: files)
+                }
                 DispatchQueue.main.async {
                     
                     self.resultFile?.wrappedValue = vaultFile
@@ -53,21 +58,36 @@ class PhotoVideoViewModel : ObservableObject {
                 //remove originalURL from phone
                 
                 if mainAppModel.importOption == .deleteOriginal {
-                                    if let originalUrl = originalUrl {
-                                        do {
-                                            print("removing file: \(originalUrl)")
-                                            try FileManager.default.removeItem(at: originalUrl)
-                                        } catch {
-                                            print("Error removing file: \(error)")
-                                        }
-                                    }
-                                }
+                    let imageUrls = [originalUrl].compactMap{$0}
+                    removeOriginalImage(imageUrls: imageUrls)
+                    
+                }
                 DispatchQueue.main.async {
                     self.resultFile?.wrappedValue = vaultFile
                 }
             }
             catch {
                 
+            }
+        }
+    }
+    
+    func removeOriginalImage(imageUrls: [URL]) {
+        PHPhotoLibrary.shared().performChanges( {
+            let imageAssetToDelete = PHAsset.fetchAssets(withALAssetURLs: imageUrls, options: nil)
+                PHAssetChangeRequest.deleteAssets(imageAssetToDelete)
+                },
+                completionHandler: { success, error in
+            print("Finished deleting asset. %@", (success ? "Success" : error as Any))
+            })
+    }
+    
+    func removeFiles(files: [URL]) {
+        for file in files {
+            do {
+                try FileManager.default.removeItem(at: file)
+            } catch {
+                print("Error deleting file: \(error.localizedDescription)")
             }
         }
     }
