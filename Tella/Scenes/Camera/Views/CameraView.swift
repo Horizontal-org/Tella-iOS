5af5bd95b0668f14ead8f1821611e89c761319fa
Robert Shrestha <robert@wearehorizontal.org>
2023-05-17 18:52:39 +0545

    Remove unnecessary codes and test codes

--- Tella/Scenes/Camera/Views/CameraView.swift
+++ Tella/Scenes/Camera/Views/CameraView.swift
@@ -95,8 +95,6 @@ struct CameraView: View {
                 guard let videoURL = videoURL else { return }
                 
                 cameraViewModel.videoURL = videoURL
-                let asset = AVAsset(url: videoURL)
-                print(asset)
                 showProgressView()
                 cameraViewModel.saveVideo()
             }
