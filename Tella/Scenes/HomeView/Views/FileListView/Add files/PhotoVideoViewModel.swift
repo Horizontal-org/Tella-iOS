0f03ef4d79ff892362c7527772f3c95e831b6fca
Robert Shrestha <robert@wearehorizontal.org>
2023-05-17 18:46:54 +0545

    Removed unnecessary files and added function comments for future

--- Tella/Scenes/HomeView/Views/FileListView/Add files/PhotoVideoViewModel.swift
+++ Tella/Scenes/HomeView/Views/FileListView/Add files/PhotoVideoViewModel.swift
@@ -25,24 +25,12 @@ class PhotoVideoViewModel : ObservableObject {
         self.folderPathArray = folderPathArray
         self.resultFile = resultFile
     }
-    
+
+    /// This function imports the video file from the user selected video with the withmetadata attached to the file
+    /// - Parameters:
+    ///   - files: Array of the URL of the videos
+    ///   - type: Type of file
     func add(files: [URL], type: FileType) {
-        Task {
-            let asset: AVAsset = AVAsset(url: files.first!)
-            asset.metadata.forEach { metadata in
-                print(metadata.key)
-                print(metadata.keySpace)
-                
-            }
-            if #available(iOS 15, *) {
-                for format in try await asset.load(.availableMetadataFormats) {
-                    _ = try await asset.loadMetadata(for: format)
-                    // Process the format-specific metadata collection.
-                }
-            } else {
-                // Fallback on earlier versions
-            }
-        }
         Task {
             
             do { let vaultFile = try await self.mainAppModel.add(files: files,
@@ -62,6 +50,10 @@ class PhotoVideoViewModel : ObservableObject {
             }
         }
     }
+    /// This function imports the video file from the user selected video with the without metadata attached to the file
+    /// - Parameters:
+    ///   - files: Array of the URL of the videos
+    ///   - type: Type of file
     func addVideoWithoutExif(files: [URL], type: FileType) {
         files.forEach { file in
             let tmpFileURL = URL(fileURLWithPath:NSTemporaryDirectory()).appendingPathComponent("\(Int(Date().timeIntervalSince1970))").appendingPathExtension(file.lastPathComponent)
@@ -86,12 +78,19 @@ class PhotoVideoViewModel : ObservableObject {
             }
         }
     }
-    
-    func addWithExif(image: UIImage , type: FileType, pathExtension:String?, originalUrl: URL?, acturalURL: URL?) {
-        guard let data = image.fixedOrientation()?.pngData() else { return }
-        let methodExifData = getEXIFData(url: acturalURL!)
+
+    /// This function imports the image file from the user selected image with the metadata attached to the file
+    /// - Parameters:
+    ///   - image: The UIImage of the image file selected
+    ///   - type: Type of the file that is selected
+    ///   - pathExtension: File extension
+    ///   - originalUrl: The original URL of the image file
+    ///   - acturalURL:  The actual URL of the image file
+    func addWithExif(image: UIImage , type: FileType, pathExtension:String?, originalUrl: URL?, actualURL: URL?) {
+        guard let data = image.pngData() else { return }
+        let methodExifData = getEXIFData(url: actualURL!)
         Task {
-            let exifData = await self.saveImageWithImageData(data: data, properties: methodExifData as NSDictionary, pathExtension: pathExtension ?? "png")
+            let exifData = await self.saveImageWithImageData(data: data, properties: methodExifData as NSDictionary)
             guard let url = mainAppModel.vaultManager.saveDataToTempFile(data: exifData as Data, pathExtension: pathExtension ?? "png") else { return  }
             do {
                 let vaultFile = try await self.mainAppModel.add(files: [url],
@@ -115,7 +114,13 @@ class PhotoVideoViewModel : ObservableObject {
         }
     }
 
-    func add(image: UIImage , type: FileType, pathExtension:String?, originalUrl: URL?, acturalURL: URL?) {
+    /// This function imports the image file from the user selected image without the metadata attached to the file
+    /// - Parameters:
+    ///   - image: The UIImage of the file
+    ///   - type: This helps to determine the file type based on enum FileType
+    ///   - pathExtension: Pathextension as  file extension
+    ///   - originalUrl: The original URL of the image file
+    func add(image: UIImage , type: FileType, pathExtension:String?, originalUrl: URL?) {
         guard let data = image.fixedOrientation()?.pngData() else { return }
         guard let url = mainAppModel.vaultManager.saveDataToTempFile(data: data, pathExtension: pathExtension ?? "png") else { return  }
         Task {
@@ -141,7 +146,13 @@ class PhotoVideoViewModel : ObservableObject {
             }
         }
     }
-    func saveImageWithImageData(data: Data, properties: NSDictionary, pathExtension: String) async -> NSData{
+
+    /// This function takes the image data and metadata as parameter and returns the image data with metadata
+    /// - Parameters:
+    ///   - data: The original image data
+    ///   - properties: The metadata that is need to be written to the image data
+    /// - Returns: The image data with metadata
+    func saveImageWithImageData(data: Data, properties: NSDictionary) async -> NSData{
 
         let imageRef: CGImageSource = CGImageSourceCreateWithData((data as CFData), nil)!
         let uti: CFString = CGImageSourceGetType(imageRef)!
@@ -152,6 +163,12 @@ class PhotoVideoViewModel : ObservableObject {
         CGImageDestinationFinalize(destination)
         return dataWithEXIF
     }
+    /// This function take the url of the video from the parameter converts it into AVAsset and exports the video file after removing the metadata to the destination URL and send the destination URL back .
+    /// If there is any issue it will return nil
+    /// - Parameters:
+    ///   - url: URL of the video file
+    ///   - destinationURL: The URL where the file without the metadata is saved
+    /// - Returns: The URL in which the file is saved or if there is any issue then it will return nil
     func exportFile(url: URL, destinationURL: URL) async -> URL? {
         let asset = AVAsset(url: url)
         print(asset.metadata)
@@ -176,6 +193,9 @@ class PhotoVideoViewModel : ObservableObject {
         }
     }
 
+    /// This function returns the EXIF or metadata as [String: Any] of the image using the URL that is passed as parameter
+    /// - Parameter url: URL of the image source
+    /// - Returns: Metadata
     func getEXIFData(url: URL) -> [String: Any] {
         let fileURL = url
         if let imageSource = CGImageSourceCreateWithURL(fileURL as CFURL, nil) {
