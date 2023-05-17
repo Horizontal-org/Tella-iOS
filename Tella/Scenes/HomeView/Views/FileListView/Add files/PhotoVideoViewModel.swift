c379d1b30beed127947c2c9aa9200dce205016b1
Robert Shrestha <robert@wearehorizontal.org>
2023-05-17 18:52:39 +0545

    Remove unnecessary codes and test codes

--- Tella/Scenes/HomeView/Views/FileListView/Add files/PhotoVideoViewModel.swift
+++ Tella/Scenes/HomeView/Views/FileListView/Add files/PhotoVideoViewModel.swift
@@ -227,31 +227,3 @@ class PhotoVideoViewModel : ObservableObject {
         }
     }
 }
-extension Data {
-    func byRemovingEXIF() -> Data? {
-        guard let source = CGImageSourceCreateWithData(self as NSData, nil),
-              let type = CGImageSourceGetType(source) else
-        {
-            return nil
-        }
-        let count = CGImageSourceGetCount(source)
-        let mutableData = NSMutableData()
-        guard let destination = CGImageDestinationCreateWithData(mutableData, type, count, nil) else {
-            return nil
-        }
-
-        let exifToRemove: CFDictionary = [
-            kCGImagePropertyExifDictionary: kCFNull,
-            kCGImagePropertyGPSDictionary: kCFNull,
-        ] as CFDictionary
-
-        for index in 0 ..< count {
-            CGImageDestinationAddImageFromSource(destination, source, index, exifToRemove)
-            if !CGImageDestinationFinalize(destination) {
-                print("Failed to finalize")
-            }
-        }
-
-        return mutableData as Data
-    }
-}
