7ff100e34a7f5ba6d7185c5971988ad3d2d41980
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2023-07-11 13:24:51 +0100

    Update toast constraints

--- Tella/Components/Toast.swift
+++ Tella/Components/Toast.swift
@@ -10,15 +10,22 @@ class Toast {
     class func displayToast(message:String) {
         
         DispatchQueue.main.async {
+            
             if let window = UIApplication.shared.keyWindow {
                 
                 let viewToShow = ToastView(message: message)
                 
                 let hostingController = UIHostingController(rootView: viewToShow)
-                hostingController.view.frame = ( window.bounds)
                 hostingController.view.backgroundColor = UIColor.clear
                 window.addSubview(hostingController.view)
-                
+
+                hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    hostingController.view.leftAnchor.constraint(equalTo: window.safeAreaLayoutGuide.leftAnchor),
+                    hostingController.view.rightAnchor.constraint(equalTo: window.safeAreaLayoutGuide.rightAnchor),
+                    hostingController.view.bottomAnchor.constraint(equalTo: window.safeAreaLayoutGuide.bottomAnchor),
+                ])
+
                 UIView.animate(withDuration: 0.5, delay: 3.0, options: UIView.AnimationOptions.curveLinear, animations: {
                     hostingController.view.alpha = 0.0
                 }) { _ in
