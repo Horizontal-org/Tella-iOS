76b2d3f2a4740c7c3de13efeff09023089393b13
Robert Shrestha <robert@wearehorizontal.org>
2023-08-16 19:31:37 +0545

    Saving the templates to db and other CRUD operations for template in db

--- Tella/Scenes/HomeView/ViewModel/HomeViewModel.swift
+++ Tella/Scenes/HomeView/ViewModel/HomeViewModel.swift
@@ -46,3 +46,10 @@ class HomeViewModel: ObservableObject {
         return recentFile
     }
 }
+extension Collection {
+
+    /// Returns the element at the specified index if it exists, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
