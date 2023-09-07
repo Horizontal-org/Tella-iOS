93494aee856d4d3f7ade3299a7bd559b15d83a9e
Gustavo <valbuena52@gmail.com>
2023-09-07 13:13:52 -0300

    isolate RenderPropertyComponent

---
+++ Tella/Scenes/Uwazi/Entity/RenderPropertyComponentView.swift
@@ -0,0 +1,21 @@
+//
+//  RenderPropertyComponentView.swift
+//  Tella
+//
+//  Created by Gustavo on 07/09/2023.
+//  Copyright © 2023 HORIZONTAL. All rights reserved.
+//
+
+import SwiftUI
+
+struct RenderPropertyComponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RenderPropertyComponentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RenderPropertyComponentView()
+    }
+}
