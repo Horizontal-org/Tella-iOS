09e47a36819a414b59a8869d0aea575ff507df09
Gustavo <valbuena52@gmail.com>
2023-09-07 13:21:37 -0300

    fix in renderPropertyComponent

--- Tella/Scenes/Uwazi/Entity/CreateEntityView.swift
+++ Tella/Scenes/Uwazi/Entity/CreateEntityView.swift
@@ -44,89 +44,23 @@ struct CreateEntityView: View {
         ScrollView {
             VStack(alignment: .leading) {
                 ForEach(entityViewModel.template.entityRow!.properties, id: \.id) { property in
-                    renderPropertyComponent(
+                    RenderPropertyComponentView(
+                        label: property.label ?? "",
                         propertyType: property.type ?? "",
-                        label: property.translatedLabel ?? "",
-                        property: property,
-                        commonProperty: nil
-                    )
+                        property: property
+                    ).environmentObject(entityViewModel)
                 }
                 ForEach(entityViewModel.template.entityRow!.commonProperties, id: \.id) { commonProperty in
-                    renderPropertyComponent(
+                    RenderPropertyComponentView(
+                        label: commonProperty.label ?? "",
                         propertyType: commonProperty.type ?? "",
-                        label: commonProperty.translatedLabel ?? "",
-                        property: nil,
                         commonProperty: commonProperty
-                    )
+                    ).environmentObject(entityViewModel)
                 }
             }.padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
         }
     }
     
-    @ViewBuilder
-    private func Title(
-        label: String
-    ) -> some View {
-        Text(label)
-            .font(.custom(Styles.Fonts.regularFontName, size: 14))
-            .foregroundColor(Color.white)
-    }
-    
-    @ViewBuilder
-    private func Subtitle(
-        label: String
-    ) -> some View {
-        Text(label)
-            .font(.custom(Styles.Fonts.regularFontName, size: 12))
-            .foregroundColor(Color.white.opacity(0.8))
-    }
-    
-    
-    @ViewBuilder
-    private func renderPropertyComponent(
-        propertyType: String,
-        label: String, property: Property?,
-        commonProperty: CommonProperty?
-    ) -> some View {
-        switch UwaziEntityPropertyType(rawValue: propertyType) {
-        case .dataTypeText, .dataTypeNumeric:
-            //render textFieldComponent
-            
-            Title(label: label)
-                            .font(.custom(Styles.Fonts.regularFontName, size: 14))
-                            .foregroundColor(Color.white)
-                        TextfieldView(
-                            fieldContent: entityViewModel.bindingForLabel(label),
-                            isValid: $entityViewModel.isValidText,
-                            shouldShowError: $entityViewModel.shouldShowError,
-                            fieldType: .text
-                        )
-        case .dataTypeDate, .dataTypeDateRange, .dataTypeMultiDate, .dataTypeMultiDateRange:
-            Text(label)
-        case .dataTypeSelect, .dataTypeMultiSelect:
-            Text(label)
-        case .dataTypeLink:
-            Text(label)
-        case .dataTypeImage:
-            Text(label)
-        case .dataTypeGeolocation:
-            Text(label)
-        case .dataTypePreview:
-            Text(label)
-        case .dataTypeMedia:
-            Text(label)
-        case .dataTypeMarkdown:
-            Text(label)
-        case .dataTypeMultiFiles, .dataTypeMultiPDFFiles:
-            Text(label)
-        case .dataTypeGeneratedID:
-            Text(label)
-        default:
-            Group {
-                Text("Unsupported property type")
-            }
-        }
-    }
 
     
     private func showSaveEntityConfirmationView() {
