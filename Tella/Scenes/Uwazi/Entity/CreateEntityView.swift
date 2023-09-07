54e95dd487c04049e125dccba208c2f556ba085d
Gustavo <valbuena52@gmail.com>
2023-09-07 13:21:37 -0300

    fix in renderPropertyComponent

--- Tella/Scenes/Uwazi/Entity/CreateEntityView.swift
+++ Tella/Scenes/Uwazi/Entity/CreateEntityView.swift
@@ -44,93 +44,23 @@ struct CreateEntityView: View {
         ScrollView {
             VStack(alignment: .leading) {
                 ForEach(entityViewModel.template.entityRow!.properties, id: \.id) { property in
-                    renderPropertyComponent(
+                    RenderPropertyComponentView(
+                        label: property.label ?? "",
                         propertyType: property.type ?? "",
-                        label: property.translatedLabel ?? "",
-                        property: property,
-                        commonProperty: nil
-                    )
+                        property: property
+                    ).environmentObject(entityViewModel)
                 }
                 ForEach(entityViewModel.template.entityRow!.commonProperties, id: \.id) { commonProperty in
-                    renderPropertyComponent(
+                    RenderPropertyComponentView(
+                        label: commonProperty.label ?? "",
                         propertyType: commonProperty.type ?? "",
-                        label: commonProperty.translatedLabel ?? "",
-                        property: nil,
                         commonProperty: commonProperty
-                    )
+                    ).environmentObject(entityViewModel)
                 }
             }.padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
         }
     }
     
-    @ViewBuilder
-    private func Title(
-        label: String
-    ) -> some View {
-        Text(label)
-            .font(.custom(Styles.Fonts.regularFontName, size: 14))
-            .foregroundColor(Color.white)
-    }
-    
-    @ViewBuilder
-    private func Subtitle(
-        label: String
-    ) -> some View {
-        Text(label)
-            .font(.custom(Styles.Fonts.regularFontName, size: 12))
-            .foregroundColor(Color.white.opacity(0.8))
-    }
-    
-    
-    @ViewBuilder
-    private func renderPropertyComponent(
-        propertyType: String,
-        label: String, property: Property?,
-        commonProperty: CommonProperty?
-    ) -> some View {
-        switch UwaziEntityPropertyType(rawValue: propertyType) {
-        case .dataTypeText, .dataTypeNumeric:
-            //render textFieldComponent
-            Title(label: label)
-                .font(.custom(Styles.Fonts.regularFontName, size: 14))
-                .foregroundColor(Color.white)
-            TextfieldView(
-                fieldContent: entityViewModel.bindingForLabel(label),
-                isValid: $entityViewModel.isValidText,
-                shouldShowError: $entityViewModel.shouldShowError,
-                fieldType: .text
-            )
-        case UwaziConstants.dataTypeDate.rawValue, UwaziConstants.dataTypeDateRange.rawValue,
-             UwaziConstants.dataTypeMultiDate.rawValue, UwaziConstants.dataTypeMultiDateRange.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypeSelect.rawValue, UwaziConstants.dataTypeMultiSelect.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypeLink.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypeImage.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypeGeolocation.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypePreview.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypeMedia.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypeMarkdown.rawValue:
-            Title(label: label)
-        case UwaziConstants.dataTypeMultiFiles.rawValue:
-            Title(label: label)
-            Subtitle(label: "Select as many files as you wish")
-        case UwaziConstants.dataTypeMultiPDFFiles.rawValue:
-            Title(label: label)
-            Subtitle(label: "Select as many PDF files as you wish")
-        case UwaziConstants.dataTypeGeneratedID.rawValue:
-            Title(label: label)
-        default:
-            Group {
-                Text("Unsupported property type")
-            }
-        }
-    }
 
     
     private func showSaveEntityConfirmationView() {
--- Tella/Scenes/Uwazi/Entity/RenderPropertyComponentView.swift
+++ Tella/Scenes/Uwazi/Entity/RenderPropertyComponentView.swift
@@ -9,13 +9,81 @@
 import SwiftUI
 
 struct RenderPropertyComponentView: View {
+    @EnvironmentObject var entityViewModel : DraftUwaziEntity
+    var label : String
+    var propertyType: String
+    var commonProperty: CommonProperty?
+    var property: Property?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        renderPropertyComponent(
+            propertyType: propertyType,
+            label: label,
+            property: property ?? nil,
+            commonProperty: commonProperty ?? nil
+        )
     }
-}
-
-struct RenderPropertyComponentView_Previews: PreviewProvider {
-    static var previews: some View {
-        RenderPropertyComponentView()
+    
+    @ViewBuilder
+    private func renderPropertyComponent(
+        propertyType: String,
+        label: String, property: Property?,
+        commonProperty: CommonProperty?
+    ) -> some View {
+        switch UwaziEntityPropertyType(rawValue: propertyType) {
+        case .dataTypeText, .dataTypeNumeric:
+            Title(label: label)
+                .font(.custom(Styles.Fonts.regularFontName, size: 14))
+                .foregroundColor(Color.white)
+            TextfieldView(
+                fieldContent:
+                    entityViewModel.bindingForLabel(label),
+                    isValid: $entityViewModel.isValidText,
+                    shouldShowError: $entityViewModel.shouldShowError,
+                    fieldType: .text
+            )
+        case .dataTypeDate, .dataTypeDateRange, .dataTypeMultiDate, .dataTypeMultiDateRange:
+            Text(label)
+        case .dataTypeSelect, .dataTypeMultiSelect:
+            Text(label)
+        case .dataTypeLink:
+            Text(label)
+        case .dataTypeImage:
+            Text(label)
+        case .dataTypeGeolocation:
+            Text(label)
+        case .dataTypePreview:
+            Text(label)
+        case .dataTypeMedia:
+            Text(label)
+        case .dataTypeMarkdown:
+            Text(label)
+        case .dataTypeMultiFiles, .dataTypeMultiPDFFiles:
+            Text(label)
+        case .dataTypeGeneratedID:
+            Text(label)
+        default:
+            Group {
+                Text("Unsupported property type")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func Title(
+        label: String
+    ) -> some View {
+        Text(label)
+            .font(.custom(Styles.Fonts.regularFontName, size: 14))
+            .foregroundColor(Color.white)
+    }
+    
+    @ViewBuilder
+    private func Subtitle(
+        label: String
+    ) -> some View {
+        Text(label)
+            .font(.custom(Styles.Fonts.regularFontName, size: 12))
+            .foregroundColor(Color.white.opacity(0.8))
     }
 }
