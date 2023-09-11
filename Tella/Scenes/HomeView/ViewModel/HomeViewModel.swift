9c7ecc64130c94c77d114b8809970e0d6d7cb0d1
Robert Shrestha <robert@wearehorizontal.org>
2023-09-11 10:04:28 +0545

    Refactor the handleTemplate method Removing unused code or dead code Removing throws that are not necessary

--- Tella/Scenes/HomeView/ViewModel/HomeViewModel.swift
+++ Tella/Scenes/HomeView/ViewModel/HomeViewModel.swift
@@ -46,10 +46,3 @@ class HomeViewModel: ObservableObject {
         return recentFile
     }
 }
-extension Collection {
-
-    /// Returns the element at the specified index if it exists, otherwise nil.
-    subscript (safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
