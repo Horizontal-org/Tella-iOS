0a539097023c33384b82954343c89f9ea7e4332a
rob-horizontal-iOS <126894298+rob-horizontal-iOS@users.noreply.github.com>
2023-09-15 20:39:51 +0545

    Change servers to connection in setting (#91)

--- Tella/Scenes/Settings/Views/Servers/ServersListView.swift
+++ Tella/Scenes/Settings/Views/Servers/ServersListView.swift
@@ -33,7 +33,7 @@ struct ServersListView: View {
         })
         
         .toolbar {
-            LeadingTitleToolbar(title: "Servers")
+            LeadingTitleToolbar(title: LocalizableSettings.settConnections.localized)
         }
     }
     
