a04eb518a4898e1dff3c47a09b25e89762667fa7
Gustavo <valbuena52@gmail.com>
2023-09-15 14:00:57 -0300

    separate responsabilities in uwaziSelectWidget

--- Tella/Scenes/Uwazi/Views/Widgets/Common/UwaziSelectWidget.swift
+++ Tella/Scenes/Uwazi/Views/Widgets/Common/UwaziSelectWidget.swift
@@ -19,55 +19,82 @@ struct UwaziSelectWidget: View {
             }
             
         } label: {
-            HStack {
-                Text(selectTitle(value:value.stringValue))
-                    .font(.custom(Styles.Fonts.regularFontName, size: 14))
-                    .foregroundColor(Color.white.opacity(0.87))
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                
-                Image("reports.arrow-down")
-                    .padding()
-                
-            }
+            SelectWidgetButton(title: selectTitle(value: value.stringValue))
         }.background(Color.white.opacity(0.08))
             .cornerRadius(12)
         
         if shouldShowMenu {
-            selectListOptions
+            SelectListOptions(prompt: prompt, shouldShowMenu: $shouldShowMenu, value: $value)
         }
     }
     
     func selectTitle(value: String) -> String {
         return value.isEmpty ? "Select" : value
     }
-    @ViewBuilder
-    var selectListOptions: some View {
+}
 
-            VStack {
-                ScrollView {
-                    VStack(spacing: 0) {
-                        ForEach(prompt.selectValues ?? [], id: \.self) { selectedOptions in
-                            Button {
-                                shouldShowMenu = false
-                                prompt.value.selectedValue = [selectedOptions]
-                                prompt.value.stringValue = selectedOptions.translatedLabel ?? ""
-                            } label: {
-                                Text(selectedOptions.translatedLabel ?? "")
-                                    .font(.custom(Styles.Fonts.regularFontName, size: 14))
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .foregroundColor(.white)
-                                    .padding(.all, 14)
-                            }.background(Color.white.opacity(0.08))
-                        }
-                    }.frame(minHeight: 40, maxHeight: 250)
-                        .background(Styles.Colors.backgroundMain)
-                        .cornerRadius(12)
+struct SelectWidgetButton: View {
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.custom(Styles.Fonts.regularFontName, size: 14))
+                .foregroundColor(Color.white.opacity(0.87))
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("reports.arrow-down")
+                .padding()
+        }
+    }
+}
+
+struct SelectListOptions: View {
+    var prompt: UwaziEntryPrompt
+    @Binding var shouldShowMenu: Bool
+    @Binding var value: UwaziValue
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack(spacing: 0) {
+                    ForEach(prompt.selectValues ?? [], id: \.self) { selectedOptions in
+                        SelectOptionButton(
+                            selectedOption: selectedOptions,
+                            shouldShowMenu: $shouldShowMenu,
+                            value: $value
+                        )
+                    }
                 }
-                Spacer()
+                .frame(minHeight: 40, maxHeight: 250)
+                .background(Styles.Colors.backgroundMain)
+                .cornerRadius(12)
             }
-            .padding()
+            Spacer()
+        }
+        .padding()
+        .background(Color.clear)
+    }
+}
 
-            .background(Color.clear)
+struct SelectOptionButton: View {
+    let selectedOption: SelectValue
+    @Binding var shouldShowMenu: Bool
+    @Binding var value: UwaziValue
+    
+    var body: some View {
+        Button(action: {
+            shouldShowMenu = false
+            value.selectedValue = [selectedOption]
+            value.stringValue = selectedOption.translatedLabel ?? ""
+        }) {
+            Text(selectedOption.translatedLabel ?? "")
+                .font(.custom(Styles.Fonts.regularFontName, size: 14))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.white)
+                .padding(.all, 14)
+        }
+        .background(Color.white.opacity(0.08))
     }
 }
