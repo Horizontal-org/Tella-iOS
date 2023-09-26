74e0a117febf7cf4af572680ac991119120c5af1
Gustavo <valbuena52@gmail.com>
2023-09-26 15:06:01 -0300

    rollback connection pageview items

--- Tella/Scenes/Reports/ReportsView.swift
+++ Tella/Scenes/Reports/ReportsView.swift
@@ -29,30 +29,60 @@ struct ReportsView: View {
     }
     
     private var contentView :some View {
-        
-        ContainerView {
-            ConnectionsPageView(
-                selectedCell: $reportsViewModel.selectedCell,
-                pageViewItems: $reportsViewModel.pageViewItems,
-                draftReports: $reportsViewModel.draftReports,
-                outboxedReports: $reportsViewModel.outboxedReports,
-                submittedReports: $reportsViewModel.submittedReports,
-                navigateToAction: { navigateTo(destination: newDraftReportView) }
-            )
-        }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: backButton)
-        
-        .if(self.reportsViewModel.selectedCell == .submitted && self.reportsViewModel.submittedReports.count > 0, transform: { view in
-            view.toolbar {
-                TrailingButtonToolbar(title: LocalizableReport.clearAppBar.localized) {
-                    showDeleteReportConfirmationView()
-                }
+            
+            ContainerView {
+                
+                VStack(alignment: .center) {
+                    
+                    PageView(selectedOption: self.$reportsViewModel.selectedCell, pageViewItems: $reportsViewModel.pageViewItems)
+                        .frame(maxWidth: .infinity, maxHeight: 40, alignment: .leading)
+                    
+                    VStack (spacing: 0) {
+                        Spacer()
+                        
+                        switch self.reportsViewModel.selectedCell {
+                        case .templates:
+                            ReportListView(reportArray: $reportsViewModel.draftReports,
+                                           message: LocalizableUwazi.uwaziTemplateListEmptyExpl.localized)
+                        case .draft:
+                            ReportListView(reportArray: $reportsViewModel.draftReports,
+                                           message: LocalizableReport.reportsDraftEmpty.localized)
+                        case .outbox:
+                            
+                            ReportListView(reportArray: $reportsViewModel.outboxedReports,
+                                           message: LocalizableReport.reportsOutboxEmpty.localized)
+                            
+                        case .submitted:
+                            ReportListView(reportArray: $reportsViewModel.submittedReports,
+                                           message: LocalizableReport.reportsSubmitedEmpty.localized)
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    TellaButtonView<AnyView> (title: LocalizableReport.reportsCreateNew.localized,
+                                              nextButtonAction: .action,
+                                              buttonType: .yellow,
+                                              isValid: .constant(true)) {
+                        navigateTo(destination: newDraftReportView)
+                    } .padding(EdgeInsets(top: 30, leading: 0, bottom: 0, trailing: 0))
+                    
+                }.background(Styles.Colors.backgroundMain)
+                    .padding(EdgeInsets(top: 15, leading: 20, bottom: 16, trailing: 20))
             }
-        })
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: backButton)
             
-            
-    }
+            .if(self.reportsViewModel.selectedCell == .submitted && self.reportsViewModel.submittedReports.count > 0, transform: { view in
+                view.toolbar {
+                    TrailingButtonToolbar(title: LocalizableReport.clearAppBar.localized) {
+                        showDeleteReportConfirmationView()
+                    }
+                }
+            })
+                
+                
+        }
     
     private var newDraftReportView: some View {
         DraftReportView(mainAppModel: mainAppModel).environmentObject(reportsViewModel)
