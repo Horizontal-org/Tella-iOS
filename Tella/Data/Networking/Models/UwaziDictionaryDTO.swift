ea722d9423a9e081c55627234400389e93ce1b9d
Gustavo <valbuena52@gmail.com>
2023-10-10 16:25:06 -0300

    add select component

--- Tella/Data/Networking/Models/UwaziDictionaryDTO.swift
+++ Tella/Data/Networking/Models/UwaziDictionaryDTO.swift
@@ -24,10 +24,25 @@ class UwaziDictionaryRowDTO: Codable {
     }
 }
 
-class SelectValue: Codable {
+class SelectValue: Codable, Equatable, Hashable {
     var label, id: String?
     var translatedLabel: String? = ""
     var values : [NestedSelectValue]? = []
+    
+    init(label: String, id: String?, translatedLabel: String?, values: [NestedSelectValue]) {
+        self.label = label
+        self.id = id
+        self.translatedLabel = translatedLabel
+        self.values = values
+    }
+
+    static func == (lhs: SelectValue, rhs: SelectValue) -> Bool {
+        return lhs.id == rhs.id && lhs.label == rhs.label
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
 }
 class NestedSelectValue: Codable {
     var id : String?
