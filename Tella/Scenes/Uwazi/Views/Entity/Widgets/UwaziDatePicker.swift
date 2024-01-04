4fa7abf255fee3ea28fddd348fc197b0d758480d
Gustavo <valbuena52@gmail.com>
2024-01-04 19:38:55 -0300

    finish UI of datePicker

--- Tella/Scenes/Uwazi/Views/Entity/Widgets/UwaziDatePicker.swift
+++ Tella/Scenes/Uwazi/Views/Entity/Widgets/UwaziDatePicker.swift
@@ -11,47 +11,48 @@ import SwiftUI
 struct UwaziDatePicker: View {
     @State private var selectedDate = Date()
     @EnvironmentObject var prompt: UwaziEntryPrompt
+    @State private var dateString: String = "Selected date"
     var body: some View {
         HStack(alignment: .lastTextBaseline) {
             ZStack(alignment: .topLeading) {
-                viewWithBackground()
+                DateLabel()
                 DatePicker("select date",
                            selection: $selectedDate,
                            displayedComponents: [.date]
                 ).datePickerStyle(.compact)
                     .labelsHidden()
                     .accentColor(Styles.Colors.lightBlue)
+                    .colorInvert()
+                    .colorMultiply(Color.clear)
                     .onChange(of: selectedDate) { newDate in
                         updatePromptWithDate(newDate)
                     }
                 
             }
-        }
+        }.frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color.white.opacity(0.08))
+            .cornerRadius(12)
     }
     
-    fileprivate func viewWithBackground() -> some View{
+    fileprivate func DateLabel() -> some View{
         ZStack {
-            Color
-                .white.opacity(0.16)
-                .cornerRadius(15)
             HStack(alignment: .top) {
                 HStack {
                     Image("uwazi.date")
-                    Text("Select Date")
+                    Text(dateString)
                         .font(.custom(Styles.Fonts.regularFontName, size: 14))
                         .foregroundColor(.white)
                 }
             }
-        }
+        }.padding()
     }
     
     private func updatePromptWithDate(_ date: Date) {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "d/MM/yyyy"
-//        prompt.value.stringValue = dateFormatter.string(from: date)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        dateString = dateFormatter.string(from: date)
         let unixTimestamp = Int(date.timeIntervalSince1970)
         prompt.value.stringValue = String(unixTimestamp)
-        dump(prompt.value)
     }
 
 }
