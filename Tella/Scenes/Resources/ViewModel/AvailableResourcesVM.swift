09514987d9adc7d4a690ecb37dd95f117d537fac
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-02-23 12:59:41 -0300

    filter downloaded resources in available list

--- Tella/Scenes/Resources/ViewModel/AvailableResourcesVM.swift
+++ Tella/Scenes/Resources/ViewModel/AvailableResourcesVM.swift
@@ -27,7 +27,8 @@ class AvailableResourcesVM: ObservableObject {
         self.servers = mainAppModel.vaultManager.tellaData?.tellaServers.value ?? []
         self.downloadedResourcesVM = downloadedVM
         self.resourceService = resourceService
-        getAvailableForDownloadResources()
+        self.getAvailableForDownloadResources()
+        self.listenDownloadDeletion()
     }
 
     func getAvailableForDownloadResources() {
@@ -50,8 +51,14 @@ class AvailableResourcesVM: ObservableObject {
 
                 },
                 receiveValue: { resources in
-                    self.availableResources.append(contentsOf: resources)
                     self.isLoading = false
+                    resources.forEach { resource in
+                        let isDownloaded = self.downloadedResourcesVM?.downloadedResources.contains(where: { $0.externalId == resource.id })
+
+                        if(!isDownloaded!) {
+                            self.availableResources.append(resource)
+                        }
+                    }
 
                 }
             ).store(in: &cancellables)
@@ -81,9 +88,18 @@ class AvailableResourcesVM: ObservableObject {
             
             if save {
                 self.downloadedResourcesVM?.fetchDownloadedResources()
+                self.availableResources.removeAll { $0.id == resource.id}
             }
         } catch {
             debugLog(error)
         }
+    }
+    
+    private func listenDownloadDeletion() {
+        self.downloadedResourcesVM?.resourceDeleted
+            .sink { [weak self] _ in
+                self?.getAvailableForDownloadResources()
+            }
+            .store(in: &cancellables)
     }
 }
