b7e16d44898b891e58140bf5fcf8f4614319f9dc
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-03-01 15:27:40 -0300

    add decoded method to download resources

--- Tella/Scenes/Resources/Models/Resource.swift
+++ Tella/Scenes/Resources/Models/Resource.swift
@@ -30,6 +30,34 @@ struct DownloadedResource: Codable, Identifiable {
     let createdAt: String
     
     enum CodingKeys: String, CodingKey {
-        case id, externalId, title, fileName, size, serverId, createdAt
+        case id = "c_id"
+        case externalId = "c_external_id"
+        case title = "c_title"
+        case fileName = "c_filename"
+        case size = "c_size"
+        case serverId = "c_server_id"
+        case createdAt = "c_created_date"
+    }
+    
+    init(id: String, externalId: String, title: String, fileName: String, size: String, serverId: Int?, createdAt: String) {
+        self.id = id
+        self.externalId = externalId
+        self.title = title
+        self.fileName = fileName
+        self.size = size
+        self.serverId = serverId
+        self.createdAt = createdAt
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try container.decode(String.self, forKey: .id)
+        self.externalId = try container.decode(String.self, forKey: .externalId)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.fileName = try container.decode(String.self, forKey: .fileName)
+        self.size = try container.decode(String.self, forKey: .size)
+        self.serverId = try container.decodeIfPresent(Int.self, forKey: .serverId)
+        self.createdAt = try container.decode(String.self, forKey: .createdAt)
     }
 }
