9ab18a39f78d36317cd521661a2f50a283d2e81d
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-03-07 17:46:05 -0300

    return list of resource domain model when fetching resources from server

--- Tella/Data/Networking/Models/ResourceDTO.swift
+++ Tella/Data/Networking/Models/ResourceDTO.swift
@@ -8,11 +8,42 @@
 
 import Foundation
 
-struct ResourceDTO: Codable {
+struct ProjectDTO: DataModel, Codable {
     let id: String
     let name: String
     let slug: String
     let url: String
-    let resources: [Resource]
+    let resources: [ResourceDTO]
     let createdAt: String
+    
+    func toDomain() -> DomainModel? {
+        let resources = resources.compactMap { $0.toDomain() as? Resource}
+        
+        return Project(
+            id: id,
+            resources: resources
+        )
+    }
+}
+
+struct ResourceDTO: Codable, Identifiable, DataModel {
+    let id: String
+    let title: String
+    let fileName: String
+    let size: String
+    let createdAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, title, fileName, size, createdAt
+    }
+    
+    func toDomain() -> DomainModel? {
+        return Resource(
+            id: id,
+            title: title,
+            fileName: fileName,
+            size: size,
+            createdAt: createdAt
+        )
+    }
 }
