65a6210767827d26dcf10ac4bb52200cef3a1220
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-04-10 19:04:57 -0300

    add placeholder

--- Tella/Components/SearchBarView.swift
+++ Tella/Components/SearchBarView.swift
@@ -25,42 +25,64 @@ struct SearchBar: View {
     @Binding var searchText: String
     @FocusState private var isInputActive:Bool
     var body: some View {
-        HStack {
-            Image("file.search")
-            TextField("", text: $searchText)
-            .keyboardType(.default)
-            .textFieldStyle(TextfieldStyle(shouldShowError: false))
-            .frame( height: 22)
-            .focused($isInputActive)
-        }
-        .padding()
-        .overlay(
-            RoundedRectangle(cornerRadius: 6)
-                .stroke(isInputActive ? Styles.Colors.yellow : .white.opacity(0.64), lineWidth: 1)
-        )
-        .padding()
+        VStack {
+            ZStack {
+                PlaceholderText(searchText: $searchText)
+                HStack {
+                    Image("file.search")
+                    TextField("", text: $searchText)
+                        .keyboardType(.default)
+                        .textFieldStyle(TextfieldStyle(shouldShowError: false))
+                        .frame(height: 22)
+                        .focused($isInputActive)
+                }
+                .padding()
+                .overlay(
+                  RoundedRectangle(cornerRadius: 6)
+                    .stroke(isInputActive ? Styles.Colors.yellow : .white.opacity(0.64), lineWidth: 1)
+                )
+                .padding()
+            }
+        }.padding(.top, 8)
     }
 }
 
 struct SearchBarNoFocus: View {
     @Binding var searchText: String
     var body: some View {
-        HStack {
-            Image("file.search")
-            TextField("", text: $searchText)
-            .keyboardType(.default)
-            .textFieldStyle(TextfieldStyle(shouldShowError: false))
-            .frame( height: 22)
-        }
-        .padding()
-        .overlay(
-            RoundedRectangle(cornerRadius: 6)
-                .stroke(.white.opacity(0.64), lineWidth: 1)
-        )
-        .padding()
+        VStack {
+            ZStack {
+                PlaceholderText(searchText: $searchText)
+                HStack {
+                    Image("file.search")
+                    TextField("", text: $searchText)
+                        .keyboardType(.default)
+                        .textFieldStyle(TextfieldStyle(shouldShowError: false))
+                        .frame( height: 22)
+                    }
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(.white.opacity(0.64), lineWidth: 1)
+                    )
+                    .padding()
+            }
+        }.padding(.top, 8)
     }
 }
 
+struct PlaceholderText: View {
+    @Binding var searchText: String
+    var body: some View {
+        Text("Search")
+            .font(.custom(Styles.Fonts.regularFontName, size: 14))
+            .offset(x: searchText.isEmpty ? 42 : 0, y: searchText.isEmpty ? 0 : -40)
+            .frame(maxWidth: .infinity,alignment: .leading)
+            .contentShape(Rectangle())
+            .foregroundColor(searchText.isEmpty ? .white : .white.opacity(0.8))
+            .padding(.horizontal, 18)
+    }
+}
 #Preview {
     SearchBarView(searchText: .constant(""))
 }
