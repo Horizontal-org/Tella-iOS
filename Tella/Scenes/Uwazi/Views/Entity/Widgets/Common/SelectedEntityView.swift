16a6468db138fa407104ae0287982909f5b4c523
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-05-13 15:49:27 -0300

    create selectedEntityView

---
+++ Tella/Scenes/Uwazi/Views/Entity/Widgets/Common/SelectedEntityView.swift
@@ -0,0 +1,67 @@
+//
+//  SelectedEntityView.swift
+//  Tella
+//
+//  Created by gus valbuena on 5/13/24.
+//  Copyright © 2024 HORIZONTAL. All rights reserved.
+//
+
+import SwiftUI
+
+struct SelectedEntityView : View {
+    @ObservedObject var prompt: UwaziRelationshipEntryPrompt
+    @EnvironmentObject var entityViewModel: UwaziEntityViewModel
+    
+    var body: some View {
+        VStack {
+            entitiesCounter
+            selectedEntitiesList
+        }
+    }
+    
+    var entitiesCounter: some View {
+        Text("\(prompt.value.count) \(prompt.value.count == 1 ? LocalizableUwazi.uwaziEntityRelationshipSingleConnection.localized : LocalizableUwazi.uwaziEntityRelationshipMultipleConnections.localized)"
+            )
+            .font(.custom(Styles.Fonts.regularFontName, size: 14))
+            .foregroundColor(Color.white.opacity(0.87))
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    var selectedEntitiesList: some View {
+        VStack {
+            ForEach(prompt.value, id: \.self) {entity in
+                entityView(for: entity)
+            }
+        }
+    }
+    
+    private func entityView(for entity: String) -> some View {
+        HStack {
+            entityIcon
+            entityLabel(for: entity)
+        }
+        .padding(.vertical, 4)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    var entityIcon: some View {
+        RoundedRectangle(cornerRadius: 5)
+            .fill(Color.white.opacity(0.2))
+            .frame(width: 35, height: 38, alignment: .center)
+            .overlay(
+                Image("files.list")
+            )
+    }
+    
+    private func entityLabel(for value: String) -> some View {
+        Text(getEntityLabel(value: value))
+            .font(.custom(Styles.Fonts.regularFontName, size: 14))
+            .foregroundColor(Color.white)
+            .lineLimit(1)
+            .padding(.horizontal, 4)
+    }
+    func getEntityLabel (value: String) -> String {
+        return entityViewModel.relationshipEntities.flatMap{ $0.values }
+            .first(where: { $0.id == value })?.label ?? ""
+    }
+}
--- Tella/Scenes/Uwazi/Views/Entity/Widgets/UwaziRelationshipWidget.swift
+++ Tella/Scenes/Uwazi/Views/Entity/Widgets/UwaziRelationshipWidget.swift
@@ -17,19 +17,11 @@ struct UwaziRelationshipWidget: View {
                 .font(.custom(Styles.Fonts.regularFontName, size: 12))
                 .foregroundColor(Color.white.opacity(0.87))
                 .frame(maxWidth: .infinity, alignment: .leading)
-            Button {
-                navigateTo(destination: EntitySelectorView(selectedValues: $prompt.value)
-                    .environmentObject(entityViewModel)
-                    .environmentObject(prompt)
-                )
-            } label: {
-                entitiesSelect
-            }
-            .background(Color.white.opacity(0.08))
-            .cornerRadius(15)
             
+            selectEntitiesButton
             if(!prompt.value.isEmpty) {
-                selectedEntities
+                SelectedEntityView(prompt: prompt)
+                    .environmentObject(entityViewModel)
             }
         }
         .onChange(of: prompt.value) { newValue in
@@ -37,6 +29,18 @@ struct UwaziRelationshipWidget: View {
         }
     }
 
+    var selectEntitiesButton : some View {
+        Button {
+            navigateTo(destination: EntitySelectorView(selectedValues: $prompt.value)
+                .environmentObject(entityViewModel)
+                .environmentObject(prompt)
+            )
+        } label: {
+            entitiesSelect
+        }
+        .background(Color.white.opacity(0.08))
+        .cornerRadius(15)
+    }
 
     var entitiesSelect: some View {
         HStack {
@@ -49,40 +53,5 @@ struct UwaziRelationshipWidget: View {
                 .foregroundColor(Color.white.opacity(0.87))
                 .frame(maxWidth: .infinity, alignment: .leading)
         }.padding(.horizontal, 16)
-    }
-    
-    var selectedEntities: some View {
-        VStack {
-            Text("\(prompt.value.count) \(prompt.value.count == 1 ? LocalizableUwazi.uwaziEntityRelationshipSingleConnection.localized : LocalizableUwazi.uwaziEntityRelationshipMultipleConnections.localized)"
-                )
-                .font(.custom(Styles.Fonts.regularFontName, size: 14))
-                .foregroundColor(Color.white.opacity(0.87))
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            VStack {
-                ForEach(prompt.value, id: \.self) {entity in
-                    HStack{
-                        RoundedRectangle(cornerRadius: 5)
-                            .fill(Color.white.opacity(0.2))
-                            .frame(width: 35, height: 38, alignment: .center)
-                            .overlay(
-                                Image("files.list")
-                            )
-                        Text(getEntityLabel(value: entity))
-                            .font(.custom(Styles.Fonts.regularFontName, size: 14))
-                            .foregroundColor(Color.white)
-                            .lineLimit(1)
-                            .padding(.horizontal, 4)
-                    }
-                    .padding(.vertical, 4)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                }
-            }
-        }
-    }
-    
-    func getEntityLabel (value: String) -> String {
-        return entityViewModel.relationshipEntities.flatMap{ $0.values }
-            .first(where: { $0.id == value })?.label ?? ""
     }
 }
