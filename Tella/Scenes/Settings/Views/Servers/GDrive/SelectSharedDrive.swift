ac14028febd4d4a799f59e6fcba596cc85d5be91
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-05-21 16:27:30 -0300

    mock shared drives

--- Tella/Scenes/Settings/Views/Servers/GDrive/SelectSharedDrive.swift
+++ Tella/Scenes/Settings/Views/Servers/GDrive/SelectSharedDrive.swift
@@ -9,22 +9,43 @@
 import SwiftUI
 
 struct SelectSharedDrive: View {
+    @State var selectedDrive: String = ""
     var body: some View {
         ContainerView {
             VStack(alignment: .leading){
-                HStack {
-                    Text("one of the drive")
-                        .font(.custom(Styles.Fonts.regularFontName, size: 15))
-                        .foregroundColor(.white)
-                    Spacer()
-                    Image("settings.done")
-                }.padding(18)
+                ForEach(SharedDrivesList, id: \.id) { drive in
+                    DriveCardView(sharedDrive: drive, selectedDrive: $selectedDrive)
+                }
 
                 Spacer()
-            }.background(Color.white.opacity(0.05))
+            }
+            .padding(.vertical, 8)
+            .background(Color.white.opacity(0.03))
             .toolbar {
                 LeadingTitleToolbar(title: "Select shared drive")
             }
+        }
+    }
+}
+
+struct DriveCardView: View {
+    var sharedDrive: SharedDrive
+    @Binding var selectedDrive: String
+    var body: some View {
+        Button(action: {
+            self.selectedDrive = sharedDrive.name
+        }) {
+            HStack {
+                Text(sharedDrive.name)
+                    .font(.custom(Styles.Fonts.regularFontName, size: 16))
+                    .foregroundColor(.white)
+                Spacer()
+                if selectedDrive == sharedDrive.name {
+                    Image("settings.done")
+                }
+            }
+            .padding(18)
+            .background(selectedDrive == sharedDrive.name ? Color.white.opacity(0.1) : Color.clear)
         }
     }
 }
