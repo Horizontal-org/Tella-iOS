500e9367a70f1199e325132595edb337b9864bab
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-06-10 16:47:27 -0300

    add state management in create folder

--- Tella/Scenes/Settings/Views/Servers/GDrive/CreateDriveFolder.swift
+++ Tella/Scenes/Settings/Views/Servers/GDrive/CreateDriveFolder.swift
@@ -16,30 +16,42 @@ struct CreateDriveFolder: View {
         ContainerView {
             VStack(spacing: 20) {
                 Spacer()
-                ServerConnectionHeaderView(
-                    title: LocalizableSettings.GDriveCreatePersonalFolderTitle.localized,
-                    subtitle: LocalizableSettings.GDriveCreatePersonalFolderDesc.localized
-                )
-                TextfieldView(fieldContent: $fieldContent,
-                              isValid: $isValid,
-                              shouldShowError: .constant(false),
-                              fieldType: .text,
-                              placeholder: LocalizableSettings.GDriveCreatePersonalFolderPlaceholder.localized)
-                    .padding(.vertical, 12)
-                    .onChange(of: fieldContent) { newValue in
-                        isValid = !newValue.isEmpty
-                    }
+                headerView
+                textField
                 Spacer()
                 bottomView
             }.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
             .navigationBarHidden(true)
+        }.onReceive(gDriveServerViewModel.$createFolderState) {createFolderState in
+            if case .error(let message) = createFolderState {
+                Toast.displayToast(message: message)
+            }
         }
+    }
+    
+    var headerView: some View {
+        ServerConnectionHeaderView(
+            title: LocalizableSettings.GDriveCreatePersonalFolderTitle.localized,
+            subtitle: LocalizableSettings.GDriveCreatePersonalFolderDesc.localized
+        )
+    }
+    
+    var textField: some View {
+        TextfieldView(fieldContent: $fieldContent,
+                      isValid: $isValid,
+                      shouldShowError: .constant(false),
+                      fieldType: .text,
+                      placeholder: LocalizableSettings.GDriveCreatePersonalFolderPlaceholder.localized)
+            .padding(.vertical, 12)
+            .onChange(of: fieldContent) { newValue in
+                isValid = !newValue.isEmpty
+            }
     }
     
     var bottomView: some View {
         BottomLockView<AnyView>(isValid: $isValid,
                                 nextButtonAction: .action,
-                                shouldHideNext: false,
+                                shouldHideNext: gDriveServerViewModel.createFolderState == .loading,
                                 shouldHideBack: false,
                                 nextAction: {
             gDriveServerViewModel.createDriveFolder(folderName: fieldContent) {
