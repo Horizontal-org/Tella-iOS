a2d6098f750e2a4f7c67d04ac87988e3e9d5fcb6
Gustavo Valbuena <valbuena52@gmail.com>
2024-06-10 17:36:35 -0300

    remove multiselect and fix scrollview on single select (#156)
    * remove multiselect and afix scrollview on single select
    
    * add multiSelect to entity parsers
    
    ---------
    
    Co-authored-by: gus valbuena <gusvalbuena@guss-MacBook-Pro.local>

--- Tella/Scenes/Uwazi/Utils/UwaziEntityParser.swift
+++ Tella/Scenes/Uwazi/Utils/UwaziEntityParser.swift
@@ -90,7 +90,7 @@ class UwaziEntityParser: UwaziEntityParserProtocol {
                                               selectValues:[] ,
                                               name: $0.name)
                 
-            case .dataTypeSelect, .dataTypeMultiSelect:
+            case .dataTypeSelect:
               
                 let selectValues = $0.values?.compactMap({SelectValues(id: $0.id ?? "", label: $0.translatedLabel ?? "")})
 
