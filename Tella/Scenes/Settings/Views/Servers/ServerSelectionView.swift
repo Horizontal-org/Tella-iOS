c6ebb3735bfe1506a8860bb69653f1d96b9bb618
dhekra-rouatbi <91288060+dhekra-rouatbi@users.noreply.github.com>
2024-08-28 20:42:05 +0100

    Select server view : Fix next button action (#176)
    * Fix next button action
    
    * Fix next button action

--- Tella/Scenes/Settings/Views/Servers/ServerSelectionView.swift
+++ Tella/Scenes/Settings/Views/Servers/ServerSelectionView.swift
@@ -12,7 +12,6 @@ struct ServerSelectionView: View {
     @EnvironmentObject var serversViewModel : ServersViewModel
     @StateObject var serverViewModel : ServerViewModel
     @EnvironmentObject var mainAppModel : MainAppModel
-    @State var selectedServerType: ServerConnectionType? = nil
     @ObservedObject var gDriveVM: GDriveAuthViewModel
     @ObservedObject var gDriveServerVM: GDriveServerViewModel
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
@@ -54,11 +53,12 @@ struct ServerSelectionView: View {
                 TellaButtonView<AnyView>(
                     title: connection.title,
                     nextButtonAction: .action,
-                    isOverlay: selectedServerType == connection.type,
+                    isOverlay: serversViewModel.selectedServerType == connection.type,
                     isValid: .constant(true),
                     action: {
-                        selectedServerType = connection.type
-                    }
+                        serversViewModel.selectedServerType = connection.type
+                        serversViewModel.shouldEnableNextButton = true
+                     }
                 )
                 .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
             }
@@ -66,12 +66,11 @@ struct ServerSelectionView: View {
     }
 
     fileprivate func bottomView() -> BottomLockView<AnyView> {
-        return BottomLockView<AnyView>(isValid: .constant(true),
+        return BottomLockView<AnyView>(isValid: $serversViewModel.shouldEnableNextButton,
                                        nextButtonAction: .action,
-                                       shouldHideNext: serversViewModel.shouldHideNextButton,
                                        shouldHideBack: true,
                                        nextAction: {
-            switch selectedServerType {
+            switch serversViewModel.selectedServerType {
             case .tella:
                 navigateToTellaWebFlow()
             case .uwazi:
@@ -81,7 +80,14 @@ struct ServerSelectionView: View {
             default:
                 break
             }
+            
+            resetView()
         })
+    }
+    
+    fileprivate func resetView() {
+        serversViewModel.selectedServerType = nil
+        serversViewModel.shouldEnableNextButton = false
     }
 
     fileprivate func navigateToTellaWebFlow() {
@@ -95,14 +101,12 @@ struct ServerSelectionView: View {
     }
     
     fileprivate func navigateToGDriveFlow() {
-        serversViewModel.shouldHideNextButton = true
         gDriveVM.handleSignIn {
             navigateTo(
                 destination: SelectDriveConnection(gDriveServerViewModel: gDriveServerVM),
                 title: LocalizableSettings.settServerGDrive.localized
             )
         }
-        serversViewModel.shouldHideNextButton = false
     }
 
     fileprivate func unavailableConnectionsView() -> some View {
