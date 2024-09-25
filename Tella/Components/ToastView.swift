4c39db04c2bcfb7cc2d8acd71cea7aa718ad053a
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2024-09-25 13:06:12 +0100

    T-iOS-476 Update toast view to align with design

--- Tella/Components/ToastView.swift
+++ Tella/Components/ToastView.swift
@@ -7,28 +7,34 @@ import SwiftUI
 struct ToastView: View {
     var message: String
     @State private var isShowing = true
-
+    
     var body: some View {
         ZStack {
             if isShowing && !message.isEmpty {
-                Text(message)
-                    .font(.custom(Styles.Fonts.regularFontName, size: 14))
-                    .foregroundColor(.black)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.white)
-                    .cornerRadius(4)
-                    .padding()
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                            withAnimation {
-                                isShowing = false
-                            }
-                        }
-                    }
+                HStack(alignment: .center) {
+                    textView
+                    Spacer()
+                }
+                .padding(16)
+                .frame(maxWidth: .infinity)
+                .background(Color.white)
+                .cornerRadius(4)
             }
         }
-        .padding()
+        .padding(16)
+    }
+    
+    var textView : some View {
+        Text(message)
+            .font(.custom(Styles.Fonts.regularFontName, size: 14))
+            .foregroundColor(.black)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    withAnimation {
+                        isShowing = false
+                    }
+                }
+            }
     }
 }
 struct ToastView_Previews: PreviewProvider {
