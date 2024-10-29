274ebe04cf4b30ada9c3253f5ae453bfb3c0681d
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2024-10-29 11:42:42 +0000

    Remove unused code

--- Tella/Scenes/Dropbox/Models/DropboxUploadProgressInfo.swift
+++ Tella/Scenes/Dropbox/Models/DropboxUploadProgressInfo.swift
@@ -32,21 +32,3 @@ class DropboxUploadProgressInfo: UploadProgressInfo {
         super.init(bytesSent: bytesSent, current: current, fileId: fileId, status: status, reportStatus: reportStatus)
     }
 }
-//
-//class FileUploadState {
-//    let fileURL: URL
-//    let fileName: String
-//    let fileId: String
-//    var sessionId: String?
-//    var offset: Int64
-//    let totalBytes: Int64
-//    
-//    init(fileURL: URL, fileName: String, fileId: String, sessionId: String?, offset: Int64, totalBytes: Int64) {
-//        self.fileURL = fileURL
-//        self.fileName = fileName
-//        self.fileId = fileId
-//        self.sessionId = sessionId
-//        self.offset = offset
-//        self.totalBytes = totalBytes
-//    }
-//}
