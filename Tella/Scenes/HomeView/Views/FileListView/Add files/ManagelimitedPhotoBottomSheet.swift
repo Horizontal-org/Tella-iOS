c38ddbf712cc69bbda6d67f482d0d868bd640130
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2024-12-19 11:50:23 +0000

    Display limited photos

---
+++ Tella/Scenes/HomeView/Views/FileListView/Add files/ManagelimitedPhotoBottomSheet.swift
@@ -0,0 +1,58 @@
+//
+//  ManagelimitedPhotoBottomSheet.swift
+//  Tella
+//
+//  Created by Dhekra Rouatbi on 19/12/2024.
+//  Copyright © 2024 HORIZONTAL. All rights reserved.
+//
+
+import SwiftUI
+import Photos
+
+struct ManagelimitedPhotoBottomSheet: View {
+    
+    let items : [ListActionSheetItem] = [ListActionSheetItem(imageName: "uwazi.add-files",
+                                                             content: LocalizableVault.limitedPhotoLibraryAdd.localized,
+                                                             type: LimitedPhotoLibraryActionType.add),
+                                         ListActionSheetItem(imageName: "settings.general",
+                                                             content: LocalizableVault.limitedPhotoLibraryChangePermission.localized,
+                                                             type: LimitedPhotoLibraryActionType.settings) ]
+    
+    var body: some View {
+        ActionListBottomSheet(items: items, headerTitle: LocalizableVault.limitedPhotoLibraryManageAccess.localized,
+                              action:  {item in
+            self.handleActions(item : item)
+        })
+    }
+    
+    private func handleActions(item: ListActionSheetItem) {
+        
+        guard let type = item.type as? LimitedPhotoLibraryActionType else { return }
+        
+        switch type {
+            
+        case .add:
+            self.dismiss {
+                showLimittedAccessUI()
+            }
+        case .settings:
+            self.dismiss {
+                self.changePhotoLibraryPermission()
+            }
+        }
+    }
+    
+    func showLimittedAccessUI() {
+        DispatchQueue.main.async {
+            PHPhotoLibrary.shared().presentLimitedLibraryPicker(from: UIApplication.getTopViewController()!)
+        }
+    }
+    
+    func changePhotoLibraryPermission() {
+        UIApplication.shared.openSettings()
+    }
+}
+
+#Preview {
+    ManagelimitedPhotoBottomSheet()
+}
