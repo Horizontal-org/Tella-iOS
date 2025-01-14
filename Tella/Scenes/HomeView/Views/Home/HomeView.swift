5b426a878549d08ce6f78c0a872f566a8daaf7ed
dhekra-rouatbi <91288060+dhekra-rouatbi@users.noreply.github.com>
2025-01-14 09:41:42 +0000

    T-iOS-393 - Make all elements in homescreen scrollable, do not keep connections and recent files fixed on the top (#222)
    * T-iOS-393 - Make all elements in homescreen scrollable, do not keep connections and recent files fixed on the top
    
    * Update function name
    
    * Fix PR comment

--- Tella/Scenes/HomeView/Views/Home/HomeView.swift
+++ Tella/Scenes/HomeView/Views/Home/HomeView.swift
@@ -19,45 +19,55 @@ struct HomeView: View {
         ContainerView {
             
             VStack() {
-                Spacer()
-                    .frame( height: !viewModel.serverDataItemArray.isEmpty ? 16 : 0 )
-                ConnectionsView()
                 
-                Spacer()
-                    .frame( height: (!viewModel.serverDataItemArray.isEmpty && viewModel.recentFiles.count > 0) ? 16 : 0 )
-                
-                if appModel.settings.showRecentFiles {
+                VStack {
                     Spacer()
-                        .frame( height: viewModel.recentFiles.count > 0 ? 16 : 0 )
-                    RecentFilesListView(recentFiles: $viewModel.recentFiles)
-                }
+                        .frame( height: !viewModel.serverDataItemArray.isEmpty ? 16 : 0 )
+                    ConnectionsView()
+                    
+                    Spacer()
+                        .frame( height: (!viewModel.serverDataItemArray.isEmpty && viewModel.recentFiles.count > 0) ? 16 : 0 )
+                    
+                    if appModel.settings.showRecentFiles {
+                        Spacer()
+                            .frame( height: viewModel.recentFiles.count > 0 ? 16 : 0 )
+                        RecentFilesListView(recentFiles: $viewModel.recentFiles)
+                    }
+                    
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    FileGroupsView(shouldShowFilesTitle: viewModel.showingFilesTitle)
+                }.scrollOnOverflow()
                 
                 Spacer()
-                    .frame(height: 30)
                 
-                FileGroupsView(shouldShowFilesTitle: viewModel.showingFilesTitle)
-                
-                if appModel.settings.quickDelete {
-                    SwipeToDeleteActionView(completion: {
-                        if(appModel.settings.deleteVault) {
-                            // removes files and folders
-                            viewModel.deleteAllVaultFiles()
-                        }
-                        
-                        if(appModel.settings.deleteServerSettings) {
-                            // remove servers connections
-                            viewModel.deleteAllServersConnection()
-                        }
-                        
-                        appViewState.resetToUnlock()
-                    })
-                }
+                quickDeleteView
             }
         }
         .onAppear{
             viewModel.getFiles()
         }
         .environmentObject(viewModel)
+    }
+    
+    @ViewBuilder
+    var quickDeleteView: some View {
+        if appModel.settings.quickDelete {
+            SwipeToDeleteActionView(completion: {
+                if(appModel.settings.deleteVault) {
+                    // removes files and folders
+                    viewModel.deleteAllVaultFiles()
+                }
+                
+                if(appModel.settings.deleteServerSettings) {
+                    // remove servers connections
+                    viewModel.deleteAllServersConnection()
+                }
+                
+                appViewState.resetToUnlock()
+            })
+        }
     }
 }
 
