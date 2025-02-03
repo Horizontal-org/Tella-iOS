1609fce5e159d7ff4a2d91fb7fdbff45e58ce639
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2025-02-03 09:22:25 +0100

    Implement p2p main view

---
+++ Tella/Utils/Localizable/LocalizablePeerToPeer.swift
@@ -0,0 +1,23 @@
+//
+//  LocalizablePeerToPeer.swift
+//  Tella
+//
+//  Created by Dhekra Rouatbi on 30/1/2025.
+//  Copyright © 2025 HORIZONTAL. All rights reserved.
+//
+
+
+
+enum LocalizablePeerToPeer: String, LocalizableDelegate {
+    
+    
+    case peerToPeerAppBar = "PeerToPeer_AppBar"
+    case nearbySharingSubhead = "PeerToPeer_NearbySharing_Subhead"
+    case nearbySharingExpl = "PeerToPeer_NearbySharing_Expl"
+    
+    case sendFiles = "PeerToPeer_NearbySharing_SendFiles"
+    case receiveFiles = "PeerToPeer_NearbySharing_ReceiveFiles"
+    
+    case learnMore = "PeerToPeer_NearbySharing_LearnMore"
+    
+}
