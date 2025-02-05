24922289565e7673d09399e731736309cbeb9dd8
rimKtarii <rim@wearehorizontal.org>
2025-02-05 13:10:08 +0100

    Add Wifi connection Screen and permessions

---
+++ Tella/Scenes/PeerToPeer/WifiConnexion/WifiConnetionViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  SSIDViewModel.swift
+//  Tella
+//
+//  Created by RIMA on 03.02.25.
+//  Copyright © 2025 HORIZONTAL. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import SystemConfiguration.CaptiveNetwork
+import CoreLocation
+
+class WifiConnetionViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var ssid: String?
+    @Published var showPermissionAlert = false
+    
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        fetchSSID()
+    }
+    
+    func fetchSSID() {
+        let status = locationManager.authorizationStatus
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            loadSSID()
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    // Handle location permission changes
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            loadSSID()
+        case .denied, .restricted:
+            showPermissionAlert = true
+        default:
+            break
+        }
+    }
+    
+    private func loadSSID() {
+        guard let interfaces = CNCopySupportedInterfaces() as? [String] else {
+            ssid = nil
+            return
+        }
+        
+        for interface in interfaces {
+            guard let networkInfo = CNCopyCurrentNetworkInfo(interface as CFString) as? [String: Any],
+                  let currentSSID = networkInfo[kCNNetworkInfoKeySSID as String] as? String else {
+                continue
+            }
+            ssid = currentSSID
+            return
+        }
+        ssid = nil
+    }
+}
