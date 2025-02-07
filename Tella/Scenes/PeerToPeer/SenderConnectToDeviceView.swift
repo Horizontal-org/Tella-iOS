b1ff52d15e750d2ab5a6fa793642eda36c2ff21a
rimKtarii <rim@wearehorizontal.org>
2025-02-07 20:22:55 +0100

    Rename Classes

---
+++ Tella/Scenes/PeerToPeer/SenderConnectToDeviceView.swift
@@ -0,0 +1,52 @@
+//
+//  ConnectToDeviceView.swift
+//  Tella
+//
+//  Created by RIMA on 05.02.25.
+//  Copyright © 2025 HORIZONTAL. All rights reserved.
+//
+
+import SwiftUI
+
+struct SenderConnectToDeviceView: View {
+    var body: some View {
+        ContainerViewWithHeader {
+            navigationBarView
+        } content: {
+            VStack {
+                RegularText(LocalizablePeerToPeer.scanCode.localized, size: 18)
+                    .padding(.top, 74)
+                
+                qrCodeView.padding(.bottom, 40)
+                RegularText(LocalizablePeerToPeer.havingTrouble.localized)
+                connectManuallyButton
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+        }
+    }
+    var navigationBarView: some View {
+        NavigationHeaderView(title: LocalizablePeerToPeer.connectToDevice.localized,
+                             navigationBarType: .inline,
+                             backButtonAction: {self.popToRoot()},
+                             rightButtonType: .none)
+    }
+    var qrCodeView: some View {
+        ResizableImage("qrCode.icon").frame(width: 248, height: 248)
+        //TODO: Add a QRCode reader
+    }
+    
+    var connectManuallyButton: some View {
+        TellaButtonView(title: LocalizablePeerToPeer.connectManually.localized.uppercased(),
+                        nextButtonAction: .destination,
+                        destination: TellaWebServerLoginView(),
+                        isValid: .constant(true),
+                        buttonRole: .secondary)
+        .padding([.leading, .trailing], 80)
+    }
+}
+
+#Preview {
+    SenderConnectToDeviceView()
+}
