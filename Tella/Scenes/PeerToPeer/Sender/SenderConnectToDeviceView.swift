b6eaea52c4e76eb26825b55c88e2a330ed596ae1
rimKtarii <rim@wearehorizontal.org>
2025-02-11 16:00:41 +0100

    Handle navigation

--- Tella/Scenes/PeerToPeer/Sender/SenderConnectToDeviceView.swift
+++ Tella/Scenes/PeerToPeer/Sender/SenderConnectToDeviceView.swift
@@ -40,7 +40,7 @@ struct SenderConnectToDeviceView: View {
     var connectManuallyButton: some View {
         TellaButtonView(title: LocalizablePeerToPeer.connectManually.localized.uppercased(),
                         nextButtonAction: .destination,
-                        destination: TellaWebServerLoginView(),
+                        destination: SenderConnectToDeviceManuallyView(viewModel: ConnectToDeviceManuallyViewModel()),
                         isValid: .constant(true),
                         buttonRole: .secondary)
         .padding([.leading, .trailing], 80)
