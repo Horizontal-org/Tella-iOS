947c52d19b29801e5c0ef1f1c7d27433da45de5f
rimKtarii <rim@wearehorizontal.org>
2025-02-25 09:58:49 +0100

    Validate fields in SenderConnectToDeviceManuallyView

--- Tella/Scenes/PeerToPeer/ConnectToDeviceManuallyViewModel.swift
+++ Tella/Scenes/PeerToPeer/ConnectToDeviceManuallyViewModel.swift
@@ -7,13 +7,38 @@
 //
 
 import Foundation
-
+import Combine
 class ConnectToDeviceManuallyViewModel: ObservableObject {
     
+    var cancellables = Set<AnyCancellable>()
+
+    init() {
+        validateFields()
+    }
     @Published var ipAddress : String = ""
     @Published var pin: String = ""
     @Published var port: String = ""
     @Published var publicKey: String = ""
     @Published var valid : Bool = false
     @Published var validFields: Bool = false
+    
+    // Fields validation
+    @Published var isValidIpAddress: Bool = false
+    @Published var isValidPin: Bool = false
+    @Published var isValidPort : Bool = false
+    
+    // Fields validation
+    @Published var shouldShowIpAddressError: Bool = false
+    @Published var shouldShowPinError: Bool = false
+
+    func validateFields() {
+        Publishers.CombineLatest3($isValidIpAddress, $isValidPin, $isValidPort)
+            .map { ip, pin, port in
+                return ip && pin && port
+            }
+            .assign(to: \.validFields, on: self)
+            .store(in: &cancellables)
+    }
+    
+
 }
