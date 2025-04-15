78f658e92b464a051569aa5fa17e46ed86c1d4ed
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2025-04-15 12:12:13 +0100

    Code improvement

---
+++ Tella/Utils/Extensions/SecKeyExtension.swift
@@ -0,0 +1,33 @@
+//
+//  SecKeyExtension.swift
+//  Tella
+//
+//  Created by Dhekra Rouatbi on 15/4/2025.
+//  Copyright © 2025 HORIZONTAL. All rights reserved.
+//
+
+import Security
+import Foundation
+
+extension SecKey {
+    func getString() -> String? {
+        guard let data = getData() else {
+            return nil
+        }
+        return data.base64EncodedString()
+    }
+    
+    func getData() -> Data? {
+        var error:Unmanaged<CFError>?
+        guard let cfdata = SecKeyCopyExternalRepresentation(self, &error) else {
+            return nil
+        }
+        let data:Data = cfdata as Data
+        return data
+    }
+    
+    func getPublicKey() -> SecKey? {
+        return SecKeyCopyPublicKey(self)
+    }
+}
+
