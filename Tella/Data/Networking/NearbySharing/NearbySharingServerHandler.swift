c36a221984e0a3693ca8eab817212d3d8752f315
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2025-08-13 19:46:06 +0100

    Move NearbySharingState into an actor to prevent concurrent access

---
+++ Tella/Data/Networking/NearbySharing/NearbySharingServerHandler.swift
@@ -0,0 +1,37 @@
+//
+//  NearbySharingServerHandler.swift
+//  Tella
+//
+//  Created by Dhekra Rouatbi on 13/8/2025.
+//  Copyright © 2025 HORIZONTAL.
+//  Licensed under MIT (https://github.com/Horizontal-org/Tella-iOS/blob/develop/LICENSE)
+//
+
+
+import Network
+import Foundation
+
+protocol PingHandler {
+    func handlePingRequest(on connection: NWConnection)
+}
+
+protocol RegisterHandler {
+    func handleRegisterRequest(on connection: NWConnection, request: HTTPRequest)
+    func generateRegisterServerResponse(from request: HTTPRequest) async throws -> NearbySharingServerResponse
+    func respondToRegistrationRequest(accept: Bool)
+}
+
+protocol PrepareUploadHandler {
+    func handlePrepareUploadRequest(on connection: NWConnection, request: HTTPRequest)
+    func respondToFileOffer(accept: Bool)
+}
+
+protocol UploadHandler {
+    func handleFileUploadRequest(on connection: NWConnection, request: HTTPRequest) async -> URL?
+    func processProgress(connection: NWConnection, bytesReceived: Int, for request: HTTPRequest)
+}
+
+protocol CloseConnectionHandler {
+    func handleCloseConnectionRequest(on connection: NWConnection, request: HTTPRequest)
+    func generateCloseConnectionResponse(from requestBody: String) async throws -> NearbySharingServerResponse
+}
