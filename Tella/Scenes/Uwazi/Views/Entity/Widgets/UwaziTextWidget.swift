81f19bb820b47e2369f6900fced7234c44aff56b
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2025-11-13 21:29:30 +0000

    Update the keyboard type for the Numeric field in Uwazi

--- Tella/Scenes/Uwazi/Views/Entity/Widgets/UwaziTextWidget.swift
+++ Tella/Scenes/Uwazi/Views/Entity/Widgets/UwaziTextWidget.swift
@@ -3,7 +3,7 @@
 //  Tella
 //
 //  Created by Gustavo on 29/09/2023.
-//  Copyright © 2023 HORIZONTAL. 
+//  Copyright © 2023 HORIZONTAL.
 //  Licensed under MIT (https://github.com/Horizontal-org/Tella-iOS/blob/develop/LICENSE)
 //
 
@@ -17,18 +17,17 @@ struct UwaziTextWidget: View {
     var body: some View {
         VStack(alignment: .leading) {
             TextField("", text: $prompt.value)
-            .keyboardType(.default)
-            .textFieldStyle(TextfieldStyle(shouldShowError: false))
-            .frame( height: 22)
-            .onChange(of: prompt.value, perform: { value in
-                prompt.showClear = !value.isEmpty
-                uwaziEntityViewModel.publishUpdates()
-            })
-
+                .textFieldStyle(TextfieldStyle(shouldShowError: false, keyboardType: prompt.type == .dataTypeNumeric ? .numberPad : .default))
+                .frame( height: 22)
+                .onChange(of: prompt.value, perform: { value in
+                    prompt.showClear = !value.isEmpty
+                    uwaziEntityViewModel.publishUpdates()
+                })
+            
             Divider()
-            .frame(height: 1)
-            .background(Color.white)
-            .opacity(0.64)
+                .frame(height: 1)
+                .background(Color.white)
+                .opacity(0.64)
         }
     }
 }
