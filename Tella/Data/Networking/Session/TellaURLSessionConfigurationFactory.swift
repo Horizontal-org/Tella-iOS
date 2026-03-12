f5b8acebb0340efd3ca2cd28a2685b13f38f46c1
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2026-03-12 10:54:32 +0000

    Revert "Display error for debug"
    This reverts commit 4c0d9c5dc10e08c0b3b8e3c77d62c9ec5c20a99f.

--- Tella/Data/Networking/Session/TellaURLSessionConfigurationFactory.swift
+++ Tella/Data/Networking/Session/TellaURLSessionConfigurationFactory.swift
@@ -17,25 +17,24 @@ protocol URLSessionConfigurationFactoryProtocol {
 }
 
 final class TellaURLSessionConfigurationFactory: URLSessionConfigurationFactoryProtocol {
-    
+
     func makeDefault() -> URLSessionConfiguration {
         let config = URLSessionConfiguration.default
-        config.waitsForConnectivity = false
-        config.timeoutIntervalForRequest = 20
+        config.waitsForConnectivity = true
+        config.timeoutIntervalForRequest = 30
         config.allowsConstrainedNetworkAccess = true
         config.allowsExpensiveNetworkAccess = true
         return config
     }
-    
+
     func makeBackground(identifier: String) -> URLSessionConfiguration {
         let config = URLSessionConfiguration.background(withIdentifier: identifier)
-        config.waitsForConnectivity = false
-        config.timeoutIntervalForRequest = 20
+        config.waitsForConnectivity = true
+        config.timeoutIntervalForRequest = 30
         config.sessionSendsLaunchEvents = true
         config.shouldUseExtendedBackgroundIdleMode = true
         config.isDiscretionary = false
         config.allowsConstrainedNetworkAccess = true
         config.allowsExpensiveNetworkAccess = true
         return config
-    }
-}
+    }}
