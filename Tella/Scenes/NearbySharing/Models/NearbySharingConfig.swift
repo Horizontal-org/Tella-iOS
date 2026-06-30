d95dbd0c81bd9967089b768fd1ee38f5cc811ce6
Dhekra Rouatbi <dhekra.rouatbi@gmail.com>
2026-06-30 13:06:05 +0100

    Pre-load port in manual connection

---
+++ Tella/Scenes/NearbySharing/Models/NearbySharingConfig.swift
@@ -0,0 +1,15 @@
+//
+//  NearbySharingConfig.swift
+//  Tella
+//
+//  Created by Dhekra Rouatbi on 30/6/2026.
+//  Copyright © 2026 HORIZONTAL.
+//  Licensed under MIT (https://github.com/Horizontal-org/Tella-iOS/blob/develop/LICENSE)
+//
+
+import Foundation
+
+enum NearbySharingConfig {
+
+    static let defaultPort = 53320
+}
